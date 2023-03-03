3d44568c3c4dbce6fa3d71bc7952dcf2019b5639
FELIX-MAIN <felix.irungu@student.moringaschool.com>
2023-03-03 22:39:10 +0300

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,45 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+
+RoomSet.create([
+    {
+        name: "Bedroom_Furniture",
+        year: 2021
+    },
+    {
+        name: "Bedroom_Interior",
+        year: 2021
+    },
+    {
+        name: "Living_Room_Partition",
+        year: 2021
+    }
+   
+])
+
+
+Room.create([
+    {
+        name: "Single",
+        size: "small",
+        units: 3,
+        room_set_id: 29,
+        img_url: "https://images.app.goo.gl/LhgC8SVUcZxLFZf37"
+    },
+    {
+        name: "Double",
+        size: "Medium",
+        units: 4,
+        room_set_id: 27,
+        img_url: "https://images.app.goo.gl/A46PBSjL7EARnJ4t5"
+    },
+    {
+        name: "Tripple",
+        size: "Medium",
+        units: 1,
+        room_set_id: 46,
+        img_url: "https://images.app.goo.gl/zn7V5JtQPaFQ5xZYA"
+    }
+])
 
 puts "✅ Done seeding!"
