8d90a772a8ee8f8d7c336d9c53104ac1f0fb0dbe
FELIX-MAIN <felix.irungu@student.moringaschool.com>
2023-03-03 23:37:09 +0300

    Done

--- server.rb
+++ server.rb
@@ -3,6 +3,13 @@ require "sinatra/activerecord"
 
 set :database, {adapter:"sqlite3", database: "development.sqlite3"}
 
+class Room < ActiveRecord::Base
+end
+
 get '/' do
   "Ola"
+end
+
+post '/rooms' do
+  @room = Room.find_or_create_by(name: params[:name])
 end
